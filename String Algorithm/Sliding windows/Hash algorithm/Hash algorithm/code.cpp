0c73a78e7e3876846efbf526404682b9f54470e3
Sadiq <62433633+sadiqshahariar@users.noreply.github.com>
2020-12-22 13:12:53 +0600

    Add files via upload

---
+++ String Algorithm/Sliding windows/Hash algorithm/Hash algorithm/code.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using  namespace  std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+
+#define faster  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+#define sq(x)   (x)*(x)
+#define PI      acos(-1.0)
+#define all(x) x.begin(),x.end()
+#define nl      '\n'
+ #define mod 1000000007
+typedef long long int ll;
+typedef unsigned long long int  llu;
+
+ll gethash(string s)
+{
+    ll ans=0;
+    ll p=31;
+    ll power=1;
+    for(char ch: s)
+    {
+        ans=(ans+(ch-'a'+1)*power)%mod;
+        power=(power*p)%mod;
+    }
+    return ans;
+}
+
+int main()
+{
+    while(1)
+    {
+        string s;
+        cin>>s;
+        cout<<gethash(s)<<nl;
+    }
+    return 0;
+}
