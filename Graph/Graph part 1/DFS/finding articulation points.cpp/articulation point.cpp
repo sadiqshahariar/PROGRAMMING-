4375b7285960ffc8c648cd251fac2acccf1b3f79
Sadiq <62433633+sadiqshahariar@users.noreply.github.com>
2021-04-02 12:37:21 +0600

    Update articulation point.cpp

--- Graph/Graph part 1/DFS/finding articulation points.cpp/articulation point.cpp
+++ Graph/Graph part 1/DFS/finding articulation points.cpp/articulation point.cpp
@@ -1,68 +1,83 @@
 #include<bits/stdc++.h>
 using  namespace  std;
-
+ 
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-
+ 
 #define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
-
+ 
 #define faster  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-
+ 
 #define sq(x)   (x)*(x)
 #define PI      acos(-1.0)
 #define all(x) x.begin(),x.end()
 #define nl      '\n'
-
+ 
 typedef long long int ll;
 typedef unsigned long long int  llu;
-int in[101],low[101],vis[101];
-vector<int>v[101];
-set<int>s;
+ 
+vector<int>v[10001];
+int ar[10001],in[10001],low[10001];
+set<int>ans;
 int timer;
-int xx=0;
+ 
 void dfs(int node,int par)
 {
-	vis[node]=1;
+	ar[node]=1;
 	in[node]=low[node]=timer++;
 	int cnt=0;
 	for(int child: v[node])
 	{
 		if(child==par)continue;
-		if(vis[child]==1)
+		if(ar[child]==1)
 		{
 			low[node]=min(low[node],in[child]);
 		}
 		else{
-			if(node==1)xx++;
 			dfs(child,node);
+			cnt++;
 			low[node]=min(low[node],low[child]);
-			if(low[child]>=in[node]&&par!=-1)
+			if(low[child]>=in[node]  &&  par!=-1)
 			{
-				//cout<<node<<" -> "<<child<<"  is a bridge"<<nl;
-				s.insert(node);
-				cnt++;
+				ans.insert(node);
 			}
 		}
 	}
-	if(par==-1&&cnt>1)s.insert(node);
+	if(par==-1 && cnt>1)ans.insert(node);
 }
 int main()
 {
-	int n,m,x,y,i;
-	cin>>n>>m;
-	while(m--)
+	int n,m,a,b;
+	while(1)
 	{
-		cin>>x>>y;
-		v[x].push_back(y);
-		v[y].push_back(x);
+		cin>>n>>m;
+		if(n==0&&m==0)break;
+		for(int i=1;i<=n;i++)
+		{
+			v[i].clear();
+			ar[i]=0;
+		}
+		timer=1;
+		ans.clear();
+		while(m--)
+		{
+			cin>>a>>b;
+			v[a].push_back(b);
+			v[b].push_back(a);
+		}
+		for(int i=1;i<=n;i++)
+		{
+			if(ar[i]==0)
+			{
+				dfs(i,-1);
+			}
+		}
+		cout<<ans.size()<<nl;
+		for(auto a:ans)
+		{
+			cout<<a<<" ";
+		}
 	}
-	dfs(1,-1);
-	if(xx>1)s.insert(1);
-	for(int a: s)
-	{
-		cout<<a<<" ";
-	}
-	cout<<nl;
 	return 0;
 }
